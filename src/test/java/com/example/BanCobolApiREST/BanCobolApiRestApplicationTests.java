582d0d75e0ba7f2985bf3b052ab4706864dcbd8c
yoelgarcialago <yoelgarcialago@gmail.com>
2025-12-25 21:11:22 +0100

    First commit

---
+++ src/test/java/com/example/BanCobolApiREST/BanCobolApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BanCobolApiREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanCobolApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
