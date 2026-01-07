b129e957477ead2ff2b5211aad69975cba4ebdb1
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-07 17:17:59 +0100

    New method to set the account number

--- src/main/java/com/example/BanCobolApiREST/Utils/StringFormatUtils.java
+++ src/main/java/com/example/BanCobolApiREST/Utils/StringFormatUtils.java
@@ -4,9 +4,14 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.security.SecureRandom;
 
 @Component
 public class StringFormatUtils {
+
+    private static final String PREFIX = "ES91";
+    private static final int TOTAL_LENGTH = 20;
+    private static final SecureRandom RANDOM = new SecureRandom();
 
     public String formatString (BigDecimal amount){
         long amountTransformed = (long) (amount.doubleValue() * 100);
@@ -25,5 +30,16 @@ public class StringFormatUtils {
         BigDecimal numberResult = new BigDecimal(programResult.trim());
         //
         return numberResult.movePointLeft(2).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public static String generateAccountNumber() {
+        int randomDigits = TOTAL_LENGTH - PREFIX.length();
+        StringBuilder sb = new StringBuilder(PREFIX);
+
+        for (int i = 0; i < randomDigits; i++) {
+            sb.append(RANDOM.nextInt(10));
+        }
+
+        return sb.toString();
     }
 }
