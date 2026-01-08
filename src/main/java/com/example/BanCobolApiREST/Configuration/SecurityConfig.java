3673aa3dea20d76a50071c0ca5f7e58879b5b2fb
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-08 17:30:49 +0100

    New bean added to the sec. config (Need to check the public routes)

--- src/main/java/com/example/BanCobolApiREST/Configuration/SecurityConfig.java
+++ src/main/java/com/example/BanCobolApiREST/Configuration/SecurityConfig.java
@@ -2,13 +2,39 @@ package com.example.BanCobolApiREST.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationWebFilter jwtWebFilter;
+
+    public SecurityConfig(JwtAuthenticationWebFilter jwtWebFilter) {
+        this.jwtWebFilter = jwtWebFilter;
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/security/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .addFilterAt(jwtWebFilter, SecurityWebFiltersOrder.AUTHENTICATION);
+
+        return http.build();
     }
 }
