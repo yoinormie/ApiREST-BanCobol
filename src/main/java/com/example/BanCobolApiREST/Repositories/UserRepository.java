f9bb12c1c50897ad8eb5fb99807a2374f3a410ff
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-08 17:05:35 +0100

    New method to take a user by its identifier

--- src/main/java/com/example/BanCobolApiREST/Repositories/UserRepository.java
+++ src/main/java/com/example/BanCobolApiREST/Repositories/UserRepository.java
@@ -2,6 +2,18 @@ package com.example.BanCobolApiREST.Repositories;
 
 import com.example.BanCobolApiREST.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("""
+        SELECT u
+        FROM User u
+        WHERE u.identifier = :identifier
+    """)
+    Optional<User> findUserByIdentifier(
+            @Param("identifier") String identifier
+    );
 }
