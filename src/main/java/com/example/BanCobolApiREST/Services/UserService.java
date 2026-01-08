9201cfe5a88a5b6eaae27dcd634cbec4f640e3f5
yoelgarcialago <yoelgarcialago@gmail.com>
2026-01-08 15:57:55 +0100

    Method modified to encrypt the upcoming pinCode

--- src/main/java/com/example/BanCobolApiREST/Services/UserService.java
+++ src/main/java/com/example/BanCobolApiREST/Services/UserService.java
@@ -4,6 +4,7 @@ import com.example.BanCobolApiREST.DTO.NewUser;
 import com.example.BanCobolApiREST.Mapper.UserMapper;
 import com.example.BanCobolApiREST.Models.User;
 import com.example.BanCobolApiREST.Repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,13 +12,16 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createNewUser (NewUser newUser){
+        newUser.setUserPin(passwordEncoder.encode(newUser.getUserPin()));
         User user = userMapper.toDefaultEntity(newUser);
         userRepository.save(user);
     }
